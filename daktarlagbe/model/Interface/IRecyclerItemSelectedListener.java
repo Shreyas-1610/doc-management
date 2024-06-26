66ac4ef9afa033912b23fcbc6b3648a55edc1d72
Shreyas Kulkarni <kshreyas93241@gmail.com>
2024-06-26 22:35:48 +0530

    Add files via upload

---
+++ daktarlagbe/model/Interface/IRecyclerItemSelectedListener.java
@@ -0,0 +1,7 @@
+package com.shreyas.daktarLagbe.model.Interface;
+
+import android.view.View;
+
+public interface IRecyclerItemSelectedListener {
+    void onItemSelectedListener(View view,int pos);
+}
---
+++ daktarlagbe/model/ShowToast.java
@@ -0,0 +1,7 @@
+package com.shreyas.daktarLagbe.model;
+
+public interface ShowToast {
+
+    public void onShowToast (String message);
+
+}
